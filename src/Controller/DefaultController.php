25998185c9b2bd62841bf3a8384e2b52c4e3a1c4
Roman Simkin <r.s.simkin@yandex.ru>
2019-02-19 21:46:41 +0300

    Changes

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,12 +3,13 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends AbstractController
 {
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        return $this->render('base.html.twig');
+        return $this->render('base.html.twig', ['mode' => $request->get('mode')]);
     }
 }
